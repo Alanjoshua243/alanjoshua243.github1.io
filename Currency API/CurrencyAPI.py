6aed896ab43507ba263cdea52c629daa7856bc54
Alanjoshua243 <156056658+Alanjoshua243@users.noreply.github.com>
2024-01-17 23:52:23 +0400

    Update CurrencyAPI.py

--- Currency API/CurrencyAPI.py
+++ Currency API/CurrencyAPI.py
@@ -1,100 +1,82 @@
-from tkinter import Tk, ttk
-from tkinter import *
-
+import tkinter as tk
+import ttkbootstrap as tb
 from PIL import Image, ImageTk
-
 import requests
-import json
 
+ApiKey = "fca_live_FHT5ujZ2nQZiDQJhfjmBfdinTSWBwVs5LwBSdLjq"
 
-#colors 
-cor0 = "#FFFFFF"  # white
-cor1 = "#333333"  # black
-cor2 = "#EB5D51"  # red
+LatestRateApiUrl = "https://api.freecurrencyapi.com/v1/latest"
 
-window = Tk()
-window.geometry('300x320')
-window.title('Converter')
-window.configure(bg=cor0)
-window.resizable(height = FALSE, width=FALSE)
-
-#frames
-top = Frame(window, width=300, height=60, bg=cor2)
-top.grid(row=0, column=0)
-
-main = Frame(window, width=300, height=260, bg=cor0)
-main.grid(row=1, column=0)
-
-def convert():
-    url = "https://currency-converter18.p.rapidapi.com/api/v1/convert"
+def AllCurrencies():
+    try:
+        response = requests.get(f"https://api.freecurrencyapi.com/v1/currencies?apikey={ApiKey}")
+        response.raise_for_status()  
+        data = response.json()
+        return list(data.get("data", {}).keys())
     
+    except requests.exceptions.RequestException as e:
+        print(f"Error getting currencies: {e}")
+        return []
 
-    currency_1 = combo1.get()
-    currency_2 = combo2.get()
-    amount = value.get()
+def ConvertCurrency():
+    amount = float(AmountSpinbox.get())
+    CurrencyFrom = CurrencyComboboxOne.get()
+    CurrencyTo = CurrencyComboboxTwo.get()
 
-    querystring = {"from":currency_1,"to":currency_2,"amount":amount}
+    try:
+        response = requests.get(f"{LatestRateApiUrl}?apikey={ApiKey}&base_currency={CurrencyFrom}&currencies={CurrencyTo}")
+        response.raise_for_status()
 
-    if currency_2 == 'USD':
-        symbol = '$'
-    elif currency_2 == 'INR':
-        symbol = '₹'
-    elif currency_2 == 'EUR':
-        symbol = '€'
-    elif currency_2 == 'BRL':
-        symbol = 'R$'
-    elif currency_2 == 'CAD':
-        symbol = 'CA $'
-        
+        data = response.json()
+        ExchangeRate = data.get("data", {}).get(CurrencyTo)
 
-    headers = {
-        'x-rapidapi-host': "currency-converter18.p.rapidapi.com",
-        'x-rapidapi-key': "90c59d6c9fmsh4599f814e2ffc92p17fc6djsndeaa0265ac61"
-        }
+        if ExchangeRate is not None:
+            ConvertedAmount = amount * ExchangeRate
+            ResultLabel.config(text=f"{amount} {CurrencyFrom} = {ConvertedAmount:.2f} {CurrencyTo}")
+        else:
+            ResultLabel.config(text="Error: Exchange rate not found.")
+    except requests.exceptions.RequestException as e:
+        ResultLabel.config(text=f"Error converting currency: {e}")
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+CurrencyGui = tk.Tk()
+CurrencyGui.title("Currency Converter")
+CurrencyGui.geometry('300x380')
+CurrencyGui.config(bg="#FFFFFF")
 
-    data = json.loads(response.text)
-    converted_amount = data["result"]["convertedAmount"]
-    formatted = symbol + " {:,.2f}".format(converted_amount)
+AppLabel = tk.Label(CurrencyGui, text="Currency Converter" , font=('Courier New', 16, 'bold'), background="#FFFFFF")
+AppLabel.place(x=35, y=145)
 
-    result['text'] = formatted
+ImagePath = "Image.png" 
+OriginalImage = Image.open(ImagePath)
+ResizedImage = OriginalImage.resize((100, 100)) 
+Image = ImageTk.PhotoImage(ResizedImage)
 
-    print(converted_amount, formatted)
+ImageLabel = tk.Label(CurrencyGui, image=Image)
+ImageLabel.place(x=100 , y= 30)
 
+AmountLabel = tk.Label(CurrencyGui, text="Amount:" , font=('Bahnschrift', 12), background="#FFFFFF")
+AmountLabel.place(x=10, y=190)
 
-#top frame
+AmountSpinbox = tb.Spinbox(CurrencyGui, from_=0, to=float('inf'), increment=1, width=41)
+AmountSpinbox.insert(tk.END, "10")
+AmountSpinbox.place(x=10, y=220)
 
-icon = Image.open('Currency API\Image.png')
-icon = icon.resize((40, 40))
-icon = ImageTk.PhotoImage(icon)
-app_name = Label(top, image = icon, compound=LEFT, text = "Currency Converter", height=5, padx=13, pady=30, anchor=CENTER, font=('Arial 16 bold'), bg=cor2, fg=cor0)
-app_name.place(x=0, y=0)
+currencies = AllCurrencies()
 
-#main frame
-result = Label(main, text = " ",width=16, height=2, pady=7, relief="solid", anchor=CENTER, font=('Ivy 15 bold'), bg=cor0, fg=cor1)
-result.place(x=50, y=10)
+CurrencyComboboxOne = tb.Combobox(CurrencyGui, values=currencies , width=18)
+CurrencyComboboxOne.set("USD")
+CurrencyComboboxOne.place(x=10, y=260)
 
-currency = ['CAD', 'BRL', 'EUR', 'INR', 'USD']
+CurrencyComboboxTwo = tb.Combobox(CurrencyGui, values=currencies, width=18)
+CurrencyComboboxTwo.set("EUR")
+CurrencyComboboxTwo.place(x=157, y=260)
 
-from_label = Label(main, text = "From", width=8, height=1, pady=0, padx=0, relief="flat", anchor=NW, font=('Ivy 10 bold'), bg=cor0, fg=cor1)
-from_label.place(x=48, y=90)
-combo1 = ttk.Combobox(main, width=8, justify=CENTER, font=("Ivy 12 bold"))
-combo1['values'] = (currency)
-combo1.place(x=50, y=115)
+ConvertButton = tb.Button(CurrencyGui, text="Convert", command=ConvertCurrency, width=43)
+ConvertButton.place(x=10, y=300)
 
-to_label = Label(main, text = "To", width=8, height=1, pady=0, padx=0, relief="flat", anchor=NW, font=('Ivy 10 bold'), bg=cor0, fg=cor1)
-to_label.place(x=158, y=90)
-combo2 = ttk.Combobox(main, width=8, justify=CENTER, font=("Ivy 12 bold"))
-combo2['values'] = (currency)
-combo2.place(x=160, y=115)
+ResultFrame = tk.Frame(CurrencyGui, background='lightgrey')
+ResultFrame.place(x=11, y=340)
 
-value = Entry(main, width=22, justify=CENTER, font=("Ivy 12 bold"), relief=SOLID)
-value.place(x=50, y=155)
-
-button = Button(main, text="Converter", width=19, padx=5, height=1, bg=cor2, fg=cor0,font=("Ivy 12 bold"), command=convert)
-button.place(x=50, y=210)
-
-
-
-window.mainloop()
+ResultLabel = tb.Label(ResultFrame, text="", width=46, background='lightgrey' , anchor='center')
+ResultLabel.pack(ipady=3.5)
+CurrencyGui.mainloop()
